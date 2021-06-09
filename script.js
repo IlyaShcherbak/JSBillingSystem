ec7cb27712661de7b984eee48a37165a1f27837f
Ilya Shcherbak <shcherbakilya@gmail.com>
2021-06-09 23:04:21 +0300

    Implement meter fields and validation

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let currentPayment = {
   id: "cold-water",
+  meterId: "",
+  previous: null,
+  current: null,
 };
 
 const payments = [
@@ -12,10 +15,16 @@ const payments = [
   }
 ];
 
+const errorMessages = {
+  invalidZero: "Вводимые значения должны быть больше 0",
+  invalidCurrentValue: "Текущие показания должны быть больше предыдущих",
+};
+let errorMessage = "";
+
 const sidebar = document.getElementById("sidebar");
 
 sidebar.onclick = function (event) {
-  if (event.target.tagName === 'BUTTON') {
+  if (event.target.tagName === "BUTTON") {
     const clickedElementID = event.target.id;
     
     if(clickedElementID !== currentPayment.id){
@@ -49,4 +58,55 @@ function showContentById(id) {
   
   // const clickedElementTitle = document.getElementById(id).innerText;
   // clickedElementContent.getElementsByClassName("title")[0].innerText = clickedElementTitle;
+}
+
+const meter = document.getElementById("utility-meter");
+
+meter.onclick = function () {
+  const selectedOptionText = meter.options[meter.selectedIndex].text;
+  
+  if(currentPayment.meterId !== selectedOptionText) {
+    currentPayment.meterId = selectedOptionText;
+    console.log(currentPayment);
+  }
+}
+
+
+const previousMeterValue = document.getElementById("previous-meter-value");
+previousMeterValue.onchange = function (event) {
+  const value = event.target.value;
+  
+  currentPayment.previous = +value;
+  validate();
+};
+
+const currentMeterValue = document.getElementById("current-meter-value");
+currentMeterValue.onchange = function (event) {
+  const value = event.target.value;
+  
+  currentPayment.current = +value;
+  validate();
+};
+
+function validate() {
+  errorMessage = "";
+  
+  if (currentPayment.previous !== null &&currentPayment.previous < 1) {
+    previousMeterValue.classList.add("invalid");
+    errorMessage = errorMessages.invalidZero;
+  }
+  if (currentPayment.current !== null && currentPayment.current < 1) {
+    currentMeterValue.classList.add("invalid");
+    errorMessage = errorMessages.invalidZero;
+  }
+  if (currentPayment.current !== null && currentPayment.current < currentPayment.previous) {
+    currentMeterValue.classList.add("invalid");
+    errorMessage = errorMessages.invalidCurrentValue;
+  }
+  if (!errorMessage) {
+    previousMeterValue.classList.remove("invalid");
+    currentMeterValue.classList.remove("invalid");
+  }
+  
+  document.getElementById("error-message").innerText = errorMessage;
 }
